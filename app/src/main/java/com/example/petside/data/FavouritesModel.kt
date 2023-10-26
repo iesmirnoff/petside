13c8a6085cdf85e34771bb05e735dfebaa973a68
mkorolev <mkorolev@mmtr.ru>
2023-10-26 18:14:12 +0300

    #13

--- app/src/main/java/com/example/petside/data/FavouritesModel.kt
+++ app/src/main/java/com/example/petside/data/FavouritesModel.kt
@@ -1,10 +1,15 @@
 package com.example.petside.data
 
+import com.google.gson.annotations.SerializedName
+
 data class FavouriteItem (
     val id: Number,
-    val image_id: String,
-    val sub_id: Any,
-    val created_at: String,
+    @SerializedName("image_id")
+    val imageId: String,
+    @SerializedName("sub_id")
+    val subId: Any,
+    @SerializedName("created_at")
+    val createdAt: String,
     val image: FavouriteImage,
 )
 
