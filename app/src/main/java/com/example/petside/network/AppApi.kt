b9d35ab308aa601eaafdba91850d96dc92f83804
mkorolev <mkorolev@mmtr.ru>
2023-10-26 18:30:39 +0300

    #4

--- app/src/main/java/com/example/petside/network/AppApi.kt
+++ app/src/main/java/com/example/petside/network/AppApi.kt
@@ -10,7 +10,7 @@ import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.*
 
-const val BASE_URL = "https://api.thecatapi.com/v1/"
+private const val BASE_URL = "https://api.thecatapi.com/v1/"
 
 interface AppApi {
     @POST("user/passwordlesssignup")
